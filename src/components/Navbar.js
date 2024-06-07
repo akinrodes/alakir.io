9013d66d451bcf8bb18be35e708ff697a9e7ca82
Claude Monwenagni <claude.monwenagni@gmail.com>
2024-06-07 23:12:20 +0000

    Update Link Blog and  Github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://alakirblog.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/akinrodes/projects"
+                href="https://github.com/akinrodes/"
                 target="_blank"
                 className="fork-btn-inner"
               >
